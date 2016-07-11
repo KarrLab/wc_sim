e436e0dfca69aeec03443f57b63922c881939aaa
Arthur Goldberg <Arthur.Goldberg@mssm.edu>
2016-07-11 12:27:16 -0400

    add command line option processing and help message make coverage testing optional

--- unittests/all.sh
+++ unittests/all.sh
@@ -3,10 +3,47 @@
 # @author: goldbera
 # run all unittests
 
+function usage {
+    if [ -n "$1" ]; then echo $1 >&2; fi
+cat <<EOF >&2
+usage: $(basename $0) 
+  run all unittests for Sequential_WC_Simulator
+  -c: perform coverage tests as well
+  -h: produce this message 
+EOF
+    exit 1
+}
+
+OPT_COMBINE=''
+while getopts ":ch" opt; do
+    case $opt in
+    c)
+        OPT_COVERAGE=1
+        ;;
+	h)
+	    usage
+	    ;;
+	\?)
+	    usage "unknown argument: -$OPTARG"
+	    ;;
+	:)
+	    usage "value required for argument: -$OPTARG"
+	    ;;
+    esac
+done
+
+shift $(($OPTIND - 1))
+
 python -m unittest discover .
 # RUN coverage tests on multiple unittests
-for test_file in test_*py; do
-    coverage run --branch --append $test_file
-done
-# don't test third party libraries 
-coverage html --omit "/Users/goldbera/Library/Python/2.7/lib/python/site-packages/rounding-0.03-py2.7.egg/rounding/*"
+if [[ -n "$OPT_COVERAGE" ]]
+then
+    # TODO: have coverage test print a summary
+    for test_file in test_*py; do
+        coverage run --branch --append $test_file
+    done
+    # don't test third party libraries 
+    # TODO: make this portable
+    coverage html --omit "/Users/goldbera/Library/Python/2.7/lib/python/site-packages/rounding-0.03-py2.7.egg/rounding/*"
+fi
+
