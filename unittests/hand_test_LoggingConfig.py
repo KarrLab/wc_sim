3270b683a8aa680b23cfe6ce4eacd7ce300cf767
Arthur Goldberg <Arthur.Goldberg@mssm.edu>
2016-07-14 08:07:03 -0400

    Add hand_test_LoggingConfig.py, a small script that tests logging output to stdout. Finding a way to automate it is on the todo list.

---
+++ unittests/hand_test_LoggingConfig.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import logging
+from Sequential_WC_Simulator.core.LoggingConfig import setup_logger
+
+for stdout in [True, False]:
+    logger_name = 'on_stdout_{}'.format( stdout )
+    setup_logger(logger_name, level=logging.DEBUG, to_stdout=stdout)
+    my_logger = logging.getLogger(logger_name)
+    my_logger.debug('On stdout == {}'.format( stdout ) )
