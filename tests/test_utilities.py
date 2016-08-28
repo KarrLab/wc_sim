e14b4233cd033c38a320ea31246573e426609574
Arthur Goldberg <Arthur.Goldberg@mssm.edu>
2016-08-28 10:10:39 -0400

    add scipy to Sequential_WC_Simulator/requirements.txt stop using assertRaisesRegex, which isn't available in py 2.7

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -8,12 +8,13 @@ class TestUtilities(unittest.TestCase):
         for test,correct in zip([ 'spc]',  'sp[c]' , 'sp[[c]' , 'sp[cxx ' , 'sp[c]x]x' ],
             [ None,  ('sp','c'), None , None , None , ]):
             if correct is None:
-                with self.assertRaisesRegex(ValueError, 
-                    re.escape("species_compartment_name must have the form "
-                        "species_id[compartment_id], but is")+".*$"):
+                with self.assertRaises(ValueError) as context:
                     get_species_and_compartment_from_name(test)
+                self.assertIn( "species_compartment_name must have the form "
+                            "species_id[compartment_id], but is", str(context.exception) )
                 
             else:
                 (species, compartment) = get_species_and_compartment_from_name( test )
                 self.assertEqual( correct, (species, compartment) )
+    # TODO(Arthur): also test that species_compartment_name( get_species_and_compartment_from_name( x ) ) == x
                 
