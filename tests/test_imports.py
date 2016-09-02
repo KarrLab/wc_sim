b2278edf8a1a0de8c2fe3617270da218756c877e
Arthur Goldberg <Arthur.Goldberg@mssm.edu>
2016-09-02 16:57:51 -0400

    generate tracebacks for import exceptions

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,22 +1,32 @@
 import unittest
 import sys
+import traceback
 
-# test some imports, which have been causing installation problems
+# test some imports which have been causing installation problems; feel free to add statements to 'imports'
 
 class TestImports(unittest.TestCase):
 
     def test_imports(self):
         imports = ["from matplotlib import pyplot, ticker", 
-            # "import no_such_package",  
+            "import no_such_package",  
             "from cobra.test import test_all",
             ]
-        failed = []
+        failures = []
+        tracebacks = []
         for cmd in imports:
             try:
                 exec( cmd )
             except ImportError as e:
-                failed.append(cmd)
+                (type, value, tb) = sys.exc_info()
+                failures.append(cmd)
+                tracebacks.append(tb)
                 sys.stderr.write( "ImportError while executing '{}': {}\n\n".format( cmd, e ) )
                 
-        if failed:
-            self.fail( msg="Failing imports: {}".format( ', '.join( [ "'" + f + "'" for f in failed] ) )  )
+        if failures:
+            failures_and_tracebacks = []
+            for (f,tb) in zip( failures, tracebacks ):
+                failures_and_tracebacks.append( 
+                    "Traceback from '{}':\n{}".format(f, ''.join( traceback.format_tb( tb ) ) ) )
+            self.fail( msg="Failing imports: {}".format( '\n'.join( failures_and_tracebacks ) )  )
+
+unittest.main()
