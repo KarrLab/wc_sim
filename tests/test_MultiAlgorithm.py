59facab40f396afdbdd9337cf38f750c24cecc14
Arthur Goldberg <Arthur.Goldberg@mssm.edu>
2016-09-08 09:25:33 -0400

    provide proper args to MultiAlgorithm.main( )

--- tests/test_MultiAlgorithm.py
+++ tests/test_MultiAlgorithm.py
@@ -3,10 +3,14 @@ import unittest
 class TestMultiAlgorithm(unittest.TestCase):
 
     def setUp(self):
+        from Sequential_WC_Simulator.multialgorithm.config import WC_SimulatorConfig
         from Sequential_WC_Simulator.multialgorithm.multi_algorithm import MultiAlgorithm
 
     def test_run(self):
-        args = "foo"
-        # TODO: RIGHT args
+        args = Namespace(FBA_time_step=WC_SimulatorConfig.DEFAULT_FBA_TIME_STEP, debug=True, 
+            end_time=None, model_filename='data',
+            output_directory=WC_SimulatorConfig.DEFAULT_OUTPUT_DIRECTORY,
+            plot=False, seed=None)
         MultiAlgorithm.main( args ) 
+
 
