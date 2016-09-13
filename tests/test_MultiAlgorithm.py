d7234a73d0766b7dd0f115922494d812ed9bc718
Arthur Goldberg <artgoldberg@gmail.com>
2016-09-13 12:52:56 -0400

    prepare to check test coverage by disabling the MultiAlgorithm test

--- tests/test_MultiAlgorithm.py
+++ tests/test_MultiAlgorithm.py
@@ -12,8 +12,9 @@ from Sequential_WC_Simulator.multialgorithm.multi_algorithm import MultiAlgorith
 
 class TestMultiAlgorithm(unittest.TestCase):
 
+    @unittest.skip("skip, as not a test")
     def test_run(self):
-    	# TODO: make model_filename more portable
+        # TODO: make model_filename more portable
         args = Namespace(FBA_time_step=WC_SimulatorConfig.DEFAULT_FBA_TIME_STEP, debug=True, 
             end_time=3*WC_SimulatorConfig.DEFAULT_FBA_TIME_STEP, 
             model_filename='./test_data/Model.xlsx',
