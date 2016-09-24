d0ada326f6792ca3fbaca3b4c57e2721577c9a22
Arthur Goldberg <artgoldberg@gmail.com>
2016-09-24 19:17:43 -0400

    remove print() in favor of the log package

--- Sequential_WC_Simulator/multialgorithm/multi_algorithm.py
+++ Sequential_WC_Simulator/multialgorithm/multi_algorithm.py
@@ -18,17 +18,18 @@ Created 2016/07/14
 
 # TODO(Arthur): provide 'returns' documentation for all return operations
 # TODO(Arthur): rename Seq_DES_WC_simulator
-# TODO(Arthur): convert 'printing =' to logging statements
-
 
 import sys
-import logging
 import argparse
 import warnings
 import errno
 
+# logging
+from Sequential_WC_Simulator.core.config import config_constants
+from log.config.config import ConfigAll
+debugging_logger = ConfigAll.setup_logger( config_constants ).get_logger( 'wc.debug.console' )
+
 from Sequential_WC_Simulator.core.config.config import SimulatorConfig
-from Sequential_WC_Simulator.core.logging_config import setup_logger_old
 from Sequential_WC_Simulator.core.utilities import ReproducibleRandom
 from Sequential_WC_Simulator.core.simulation_object import EventQueue, SimulationObject
 from Sequential_WC_Simulator.core.simulation_engine import SimulationEngine, MessageTypesRegistry
@@ -62,7 +63,6 @@ class MultiAlgorithm( object ):
             default=WC_SimulatorConfig.DEFAULT_FBA_TIME_STEP )
 
         output_options = parser.add_mutually_exclusive_group()
-        output_options.add_argument( '--debug', '-d', action='store_true', help='Print debug output' )
         parser.add_argument( '--seed', '-s', type=int, help='Random number seed; if not provided '
             'then reproducibility determined by SimulatorConfig.REPRODUCIBLE_SEED' )
         args = parser.parse_args()
@@ -88,10 +88,9 @@ class MultiAlgorithm( object ):
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             # 0. read model description
-            printing =  "Reading model from '{}'".format( args.model_filename, debug=args.debug ) 
-            the_model = getModelFromExcel( args.model_filename, debug_option=args.debug )
-            printing =  the_model.summary()
-            print(printing)
+            debugging_logger.info( "Reading model from '{}'".format( args.model_filename ) )
+            the_model = getModelFromExcel( args.model_filename )
+            debugging_logger.debug( the_model.summary() )
             
             '''Prepare submodels for computation'''
             the_model.setupSimulation()
@@ -102,29 +101,29 @@ class MultiAlgorithm( object ):
         for submodel_spec in the_model.submodels:
             if submodel_spec.algorithm == 'SSA':
                 if submodel_spec.algorithm in algs_to_run:
-                    printing = "create SSA submodel: {} {}".format( submodel_spec.name, 
-                        submodel_spec.algorithm )
+                    debugging_logger.info( "create SSA submodel: {} {}".format( submodel_spec.name, 
+                        submodel_spec.algorithm ) )
                     submodel_spec.the_submodel = simple_SSA_submodel( the_model, submodel_spec.name,
                         submodel_spec.id, None, shared_cell_states, submodel_spec.reactions, 
-                        submodel_spec.species, debug=args.debug )
+                        submodel_spec.species )
             elif submodel_spec.algorithm == 'FBA':
                 if submodel_spec.algorithm in algs_to_run:
-                    printing = "create FBA submodel: {} {}".format( submodel_spec.name, 
-                        submodel_spec.algorithm )
+                    debugging_logger.info( "create FBA submodel: {} {}".format( submodel_spec.name, 
+                        submodel_spec.algorithm ) )
                     submodel_spec.the_submodel = FbaSubmodel( the_model, submodel_spec.name, 
                         submodel_spec.id, None, shared_cell_states, submodel_spec.reactions,
-                        submodel_spec.species, args.FBA_time_step, debug=args.debug )
+                        submodel_spec.species, args.FBA_time_step )
             else:
                 raise Exception("Undefined algorithm '{}' for submodel '{}'".format(
                     submodel_spec.algorithm, submodel_spec.name ) )
 
         # 2. run simulation
         if args.end_time:
-            printing =  "Simulating to: {}".format( args.end_time ) 
+            debugging_logger.info( "Simulating to: {}".format( args.end_time ) )
             SimulationEngine.simulate( args.end_time )
         else:
-            printing =  "Simulating to cellCycleLength: {}".format( 
-                the_model.getComponentById('cellCycleLength').value )
+            debugging_logger.info( "Simulating to cellCycleLength: {}".format( 
+                the_model.getComponentById('cellCycleLength').value ) )
             SimulationEngine.simulate( the_model.getComponentById('cellCycleLength').value )
         
         return the_model
@@ -132,7 +131,7 @@ class MultiAlgorithm( object ):
 if __name__ == '__main__':
     try:
         args = MultiAlgorithm.parseArgs()
-        theModel = MultiAlgorithm.main( args )
+        the_model = MultiAlgorithm.main( args )
         timeHist, speciesCountsHist = the_model.the_SharedMemoryCellState.report_history(
             numpy_format=True )
         volume = None
