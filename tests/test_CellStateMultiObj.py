cc39f0f84d237334523a112a26df0c0931d77e76
Arthur Goldberg <artgoldberg@gmail.com>
2016-10-04 08:31:35 -0400

    change name of TestSimulationObject class to MockSimulationObject to avoid pytest warning "cannot collect test class 'TestSimulationObject' because it has a __init__ constructor"

--- tests/test_CellStateMultiObj.py
+++ tests/test_CellStateMultiObj.py
@@ -1,7 +1,7 @@
 '''
 Use multiple simulation objects (curently 3) to test the simulator. One object (a 
 UniversalSenderReceiverSimulationObject) sends initialization events. Another object, a CellState, manages the
-population of one species, 'x'. And another object, a TestSimulationObject, monitors the population of x and 
+population of one species, 'x'. And another object, a MockSimulationObject, monitors the population of x and 
 compares the correct population with the simulated population.
 
 '''
@@ -39,30 +39,30 @@ def parse_population_history( pop_history ):
         pop_history_dict[Event][Time] = (Pop_adjust, Flux, Population)
     return pop_history_dict
     
-class TestSimulationObject(SimulationObject):
+class MockSimulationObject(SimulationObject):
 
     SENT_MESSAGE_TYPES = [ GetPopulation ]
-    MessageTypesRegistry.set_sent_message_types( 'TestSimulationObject', SENT_MESSAGE_TYPES )
+    MessageTypesRegistry.set_sent_message_types( 'MockSimulationObject', SENT_MESSAGE_TYPES )
 
     # at any time instant, process messages in this order
     MESSAGE_TYPES_BY_PRIORITY = [ GivePopulation ]
-    MessageTypesRegistry.set_receiver_priorities( 'TestSimulationObject', MESSAGE_TYPES_BY_PRIORITY )
+    MessageTypesRegistry.set_receiver_priorities( 'MockSimulationObject', MESSAGE_TYPES_BY_PRIORITY )
     
     def __init__( self, name, pop_history, specie, debug=False):
         self.debug = debug
         self.pop_history_dict = parse_population_history( pop_history )
         self.specie = specie
-        super(TestSimulationObject, self).__init__( name )
+        super(MockSimulationObject, self).__init__( name )
 
     def handle_event( self, event_list ):
-        super( TestSimulationObject, self).handle_event( event_list )
+        super( MockSimulationObject, self).handle_event( event_list )
 
         '''
         Test for the population of x, at various times in a sequence of ADJUST_POPULATION_BY_* events. These 
         events are described in pop_history and pop_history_dict and scheduled for the CellState object by
         test_CellState_with_other_SimObj() below. 
         test_CellState_with_other_SimObj() also schedules GetPopulation events sent by TestSimObj, 
-        an instance of this TestSimulationObject, at the times in the sequence.
+        an instance of this MockSimulationObject, at the times in the sequence.
         '''
         
         for event_message in event_list:
@@ -75,12 +75,12 @@ class TestSimulationObject(SimulationObject):
                 if self.specie in populations.population:
                     if event_message.event_time in self.pop_history_dict['get_pop']:
                         (Pop_adjust, Flux, correct_pop) = self.pop_history_dict['get_pop'][event_message.event_time]
-                        # Key point: TestSimulationObject.TestCaseRef is a reference to a unittest.TestCase, 
+                        # Key point: MockSimulationObject.TestCaseRef is a reference to a unittest.TestCase, 
                         # which is set by TestSimulation.testSimulation( ) below
                         # This test works for any sequence of the stochastic rounding because either round matches
                         # on 2016/07/20 I saw a single, non-reproducible (in over 10,000 attempts) failure of this test; 
                         # the msg will make such a failure be more easily tracked if it reappears
-                        TestSimulationObject.TestCaseRef.assertTrue( 
+                        MockSimulationObject.TestCaseRef.assertTrue( 
                             populations.population[self.specie] == math.ceil(correct_pop) or
                             populations.population[self.specie] == math.floor(correct_pop), 
                             msg="At event_time {} for specie: '{}': with rounding, the correct population "
@@ -120,9 +120,9 @@ Time                 Event                Pop_adjust           Flux             
         cs1 = _CellStateMaker.make_CellState( { specie: init_pop }, {specie: init_flux} )
 
 
-        TestSimObj = TestSimulationObject( 'TestSimObj', pop_history_of_x, specie )
-        # give TestSimulationObject.TestCaseRef a reference to this unittest.TestCase:
-        TestSimulationObject.TestCaseRef = self
+        TestSimObj = MockSimulationObject( 'TestSimObj', pop_history_of_x, specie )
+        # give MockSimulationObject.TestCaseRef a reference to this unittest.TestCase:
+        MockSimulationObject.TestCaseRef = self
 
         usr = UniversalSenderReceiverSimulationObject( 'usr1' )
     
@@ -157,9 +157,9 @@ Time                 Event                Pop_adjust           Flux             
 2                    get_pop              NA                   NA                   1'''
         specie = 'y'
         pop_history_dict = parse_population_history( pop_history_of_y )
-        TestSimObj = TestSimulationObject( 'TestSimObj', pop_history_of_y, specie )
-        # give TestSimulationObject.TestCaseRef a reference to this unittest.TestCase:
-        TestSimulationObject.TestCaseRef = self
+        TestSimObj = MockSimulationObject( 'TestSimObj', pop_history_of_y, specie )
+        # give MockSimulationObject.TestCaseRef a reference to this unittest.TestCase:
+        MockSimulationObject.TestCaseRef = self
 
         usr = UniversalSenderReceiverSimulationObject( 'usr1' )
 
