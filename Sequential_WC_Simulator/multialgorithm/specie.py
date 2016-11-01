fbe861f93de43f3c8fe0a3aab29b35eecced3f88
Arthur Goldberg <artgoldberg@gmail.com>
2016-11-01 16:52:01 -0400

    add NegativePopulationError exceptions for reporting negative populations; have all methods in Specie return the updated population make time a required update docstrings for methods; make docstrings adhere to the Google Style supported by Sphinx Napoleon

--- Sequential_WC_Simulator/multialgorithm/specie.py
+++ Sequential_WC_Simulator/multialgorithm/specie.py
@@ -1,6 +1,9 @@
-''' 
-@author: Arthur Goldberg, Arthur.Goldberg@mssm.edu
-@date 7/19/2016
+'''Track the population of a single specie.
+
+:Author: Arthur Goldberg, Arthur.Goldberg@mssm.edu
+:Date: 2016-07-19
+:Copyright: 2016, Karr Lab
+:License: MIT
 '''
 
 from Sequential_WC_Simulator.multialgorithm.config import paths as config_paths
@@ -11,27 +14,27 @@ config = ConfigManager(config_paths.core).get_config()['Sequential_WC_Simulator'
 
 
 class Specie(object):
-    """ Specie tracks the population of a single specie.
-    
+    '''Specie tracks the population of a single specie.
+
     We have these cases. Suppose that the specie's population is adjusted by:
 
     * DISCRETE model only: estimating the population obtains the last value written
-    * CONTINUOUS model only: estimating the population obtains the last value written plus a linear interpolated change 
+    * CONTINUOUS model only: estimating the population obtains the last value written plus a linear interpolated change
       since the last continuous adjustment
-    * Both model types: reading the populations obtains the last value written plus a linear interpolated change 
+    * Both model types: reading the populations obtains the last value written plus a linear interpolated change
       based on the last continuous adjustment
 
     Without loss of generality, we assume that all species can be adjusted by both model types and that at most
-    one continuous model adjusts a specie's population. (Multiple continuous models of a specie - or any 
-    model attribute - would be non-sensical, as it implies multiple conflicting, simultaneous rates of change.) 
-    We also assume that if a population is adjusted by a continuous model then the adjustments occur sufficiently 
+    one continuous model adjusts a specie's population. (Multiple continuous models of a specie - or any
+    model attribute - would be nonsensical, as it implies multiple conflicting, simultaneous rates of change.)
+    We also assume that if a population is adjusted by a continuous model then the adjustments occur sufficiently
     frequently that the last adjustment alway provides a useful estimate of flux. Adjustments take the following form:
-    
+
     * DISCRETE adjustment: (time, population_change)
     * CONTINUOUS adjustment: (time, population_change, flux_estimate_at_time)
-    
+
     A Specie stores the (time, population) of the most recent adjustment and (time, flux) of the most
-    recent continuous adjustment. Naming these R_time, R_pop, C_time, and C_flux, the population p at time t is 
+    recent continuous adjustment. Naming these R_time, R_pop, C_time, and C_flux, the population p at time t is
     estimated by:::
 
         interpolation = 0
@@ -40,42 +43,41 @@ class Specie(object):
         p = R_pop + interpolation
 
     This approach is completely general, and can be applied to any simulation value.
-    
+
     Real versus integer copy numbers: Clearly, specie copy number values are non-negative integers. However,
     continuous models may estimate copy numbers as real number values. E.g., ODEs calculate real valued concentrations.
-    But SSA models do not naturally handle non-integral copy numbers, and models that represent the state of 
+    But SSA models do not naturally handle non-integral copy numbers, and models that represent the state of
     individual species -- such as a bound molecule -- are not compatible with non-integral copy numbers. Therefore,
-    Specie stores a real valued population, but reports only a non-negative integral population. 
-    In particular, the population reported by get_population() is rounded. Rounding is done  stochastically to avoid the 
+    Specie stores a real valued population, but reports only a non-negative integral population.
+    In particular, the population reported by get_population() is rounded. Rounding is done  stochastically to avoid the
     bias that would arise from always using floor() or ceiling(), especially with small populations.
-    
+
     Attributes:
-        last_population: population after the most recent adjustment
-        continuous_flux: flux provided by the most recent adjustment by a continuous model, 
+        last_population (float): population after the most recent adjustment
+        continuous_flux (float): flux provided by the most recent adjustment by a continuous model,
             if there has been an adjustment by a continuous model; otherwise, uninitialized
         continuous_time: time of the most recent adjustment by a continuous model; None if the specie has not
             received a continuous adjustment
-            
+
         Specie objects do not include the specie's name, as we assume they'll be stored in structures
         which contain the names.
-    
-    # TODO(Arthur): optimization: put Specie functionality into CellState and SharedMemoryCellState, avoiding 
+
+    # TODO(Arthur): optimization: put Specie functionality into CellState and SharedMemoryCellState, avoiding
         overhead of a Class instance for each Specie.
-    """
+    '''
     # use __slots__ to save space
     __slots__ = "specie_name last_population continuous_time continuous_flux stochasticRounder".split()
 
     def __init__( self, specie_name, initial_population, initial_flux=None ):
-        """Initialize a Specie object.
-        
+        '''Initialize a Specie object.
+
         Args:
-            specie_name: string; the specie's name; not logically needed, but may be helpful for logging, debugging, etc.
-            initial_population: non-negative number; initial population of the specie
-            initial_flux: number; required for Specie whose population is partially estimated by a 
-                continuous mode; initial flux for the specie
-        """
-        
-        # TODO(Arthur): perhaps: add optional arg to not round copy number values reported
+            specie_name (str): the specie's name; not logically needed, but helpful for error
+                reporting, logging, debugging, etc.
+            initial_population (int): non-negative number; initial population of the specie
+            initial_flux (number, optional): initial flux for the specie; required for Species whose
+                population is estimated, at least in part, by a continuous model
+        '''
         assert 0 <= initial_population, '__init__(): population should be >= 0'
         self.specie_name = specie_name
         self.last_population = initial_population
@@ -87,61 +89,88 @@ class Specie(object):
 
         self.stochasticRounder = StochasticRound( ReproducibleRandom.get_numpy_random() ).Round
 
-    def discrete_adjustment( self, population_change ):
-        """A discrete model adjusts the specie's population.
+    def discrete_adjustment( self, population_change, time ):
+        '''Make a discrete adjustment of the specie's population.
+
+        A discrete-time sub-model, such as the stochastic simulation algorithm (SSA), must use this
+        method to adjust the specie's population.
 
         Args:
-            population_change: number; modeled increase or decrease in the specie's population
-            
+            population_change (number): the modeled increase or decrease in the specie's population
+            time (number): the simulation time at which the predicted change occurs; this time is
+                used by `get_population` to interpolate continuous-time predictions between integrations.
+
+        Returns:
+            number: the specie's adjusted population
+
         Raises:
-            ValueError: if population goes negative
-        """
-        if self.last_population + population_change < 0:
-            raise ValueError( "discrete_adjustment(): negative population: "
-                "last_population + population_change = {:.2f} + {:.2f}".format( 
-                self.last_population, population_change ) )
+            NegativePopulationError: if the predicted population at `time` is negative or
+            if decreasing the population by `population_change` would make the population negative
+        '''
+        current_population = self.get_population( time )
+        if current_population + population_change < 0:
+            raise NegativePopulationError('discrete_adjustment', self.specie_name,
+                self.last_population, population_change)
         self.last_population += population_change
-    
+        return self.get_population( time )
+
     def continuous_adjustment( self, population_change, time, flux ):
-        """A continuous model adjusts the specie's population.
+        '''A continuous-time sub-model adjusts the specie's state.
+
+        A continuous-time sub-model, such as an ordinary differential equation (ODE) or a dynamic flux
+        balance analysis (FBA) model, must use this method to adjust the specie's state. We assume that
+        each integration of a continuous-time model predicts a specie's population and the
+        population's short-term future rate of change, i.e., its `flux`. Further, since an
+        integration of a continuous-time model at the current time must depend on this specie's
+        population just before the integration, we assume that population changes predicted by the
+        flux provided by the previous `continuous_adjustment` call are incorporated in this call's
+        `population_change`.
 
         Args:
-            population_change: number; modeled increase or decrease in the specie's population
-            
+            population_change (number): modeled increase or decrease in the specie's population
+            time (number): the simulation time at which the predicted change occurs; this time is
+                used by `get_population` to interpolate continuous-time predictions between integrations.
+            flux (number): the predicted flux of the specie at the provided time
+
+        Returns:
+            number: the specie's adjusted population
+
         Raises:
-            ValueError: if initial flux was not provided for a continuously updated variable
-            ValueError: if time is <= the time of the most recent continuous adjustment
-            ValueError: if population goes negative
-            AssertionError: if the time is negative
-        """
+            ValueError: if flux was not provided when this `Specie` was instantiated
+            ValueError: if `time` is not greater than the time of the most recent
+                `continuous_adjustment` call on this `Specie`
+            NegativePopulationError: if applying `population_change` makes the population go negative
+        '''
         if self.continuous_time == None:
             raise ValueError( "continuous_adjustment(): initial flux was not provided" )
-        assert 0 <= time, 'negative time: {:.2f}'.format( time )
-        # multiple continuous adjustments at a time that does not advance the specie's state do not make sense
+        # the simulation time must advance between adjacent continuous adjustments
         if time <= self.continuous_time:
-            raise ValueError( "continuous_adjustment(): time <= self.continuous_time: {:.2f} < {:.2f}".format( 
+            raise ValueError( "continuous_adjustment(): time <= self.continuous_time: {:.2f} < {:.2f}".format(
                 time, self.continuous_time ) )
         if self.last_population + population_change < 0:
-            raise ValueError( "continuous_adjustment(): negative population: "
-                "last_population + population_change = {:.2f} + {:.2f}".format( 
-                self.last_population, population_change ) )
+            raise NegativePopulationError('continuous_adjustment', self.specie_name,
+                self.last_population, population_change, time-self.continuous_time)
         self.continuous_time = time
         self.continuous_flux = flux
         self.last_population += population_change
-    
+        return self.last_population
+
     def get_population( self, time=None ):
-        """Get the specie's population at time.
-        
-        Interpolate continuous values as described in the documentation of class Specie.
-        
+        '''Obtain the specie's population.
+
+        Obtain the specie's current population. If one of the sub-model(s) predicting the specie's
+        population is a continuous-time model, then use the specie's last flux to interpolate
+        the current population as described in the documentation of this class.
+
         Args:
-            time: number; optional; simulation time of the request; 
-                time is required if the specie has had a continuous adjustment
-            
+            time (number, optional): the current simulation time; `time` is required if one of the
+                sub-models modeling the specie is a continuous-time sub-model.
+
         Raises:
-            ValueError: if time is required and not provided
-            ValueError: time is earlier than a previous continuous adjustment
-        """
+            ValueError: if `time` is required but not provided
+            ValueError: if `time` is earlier than the time of a previous continuous adjustment
+            NegativePopulationError: if interpolation predicts a negative population
+        '''
         if self.continuous_time == None:
             return self.stochasticRounder( self.last_population )
         else:
@@ -149,16 +178,64 @@ class Specie(object):
                 raise ValueError( "get_population(): time needed because "
                     "continuous adjustment received at time {:.2f}".format( self.continuous_time ) )
             if time < self.continuous_time:
-                raise ValueError( "get_population(): time < self.continuous_time: {:.2f} < {:.2f}\n".format( 
+                raise ValueError( "get_population(): time < self.continuous_time: {:.2f} < {:.2f}\n".format(
                     time, self.continuous_time ) )
             interpolation=0
             if config['interpolate']:
                 interpolation = (time - self.continuous_time) * self.continuous_flux
+            if self.last_population + interpolation < 0:
+                raise NegativePopulationError('get_population', self.specie_name,
+                    self.last_population, interpolation, time - self.continuous_time)
             float_copy_number = self.last_population + interpolation
             return self.stochasticRounder( float_copy_number )
-            
+
     def __str__( self ):
-        return "specie_name:{}; last_population:{}; continuous_time:{}; continuous_flux:{}".format( 
+        return "specie_name:{}; last_population:{}; continuous_time:{}; continuous_flux:{}".format(
             self.specie_name, self.last_population, self.continuous_time, self.continuous_flux )
 
-        
+class Error(Exception):
+    '''Base class for exceptions in Specie.'''
+    pass
+
+class NegativePopulationError(Error):
+    '''Exception raised when a negative specie population is predicted.
+
+    `last_population` plus `population_decrease` equals the predicted negative population.
+
+    Attributes:
+        method (:obj:`str`): name of the method in which the exception occured
+        specie (:obj:`str`): name of the specie whose population has become negative
+        last_population (:obj:`float`): last population of the specie
+        population_decrease (:obj:`float`): change to the population which makes it negative
+        delta_time (:obj:`float`, optional): if the specie has been updated by a continuous submodel,
+            time since the last continuous update
+    '''
+    def __init__(self, method, specie, last_population, population_decrease, delta_time=None):
+        self.method=method
+        self.specie=specie
+        self.last_population=last_population
+        self.population_decrease=population_decrease
+        self.delta_time=delta_time
+
+    def __eq__(self, other):
+        '''Override the default Equals behavior'''
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        return False
+
+    def __ne__(self, other):
+        '''Define a non-equality test'''
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return hash( (self.method, self.specie, self.last_population, self.population_decrease,
+            self.delta_time))
+
+    def __str__(self):
+        '''string representation'''
+        rv = "{}(): negative population for '{}', with decline from {:g} to {:g}".format( self.method,
+                self.specie, self.last_population, self.last_population+self.population_decrease )
+        if self.delta_time is None:
+            return rv
+        else:
+            return rv + " over {:g} time units".format(self.delta_time)
--- tests/test_Specie.py
+++ tests/test_Specie.py
@@ -1,3 +1,10 @@
+''' Test specie.py.
+
+:Author: Arthur Goldberg, Arthur.Goldberg@mssm.edu
+:Date: 2016-10-01
+:Copyright: 2016, Karr Lab
+:License: MIT
+'''
 import unittest
 import sys
 import re
@@ -7,57 +14,48 @@ from wc_utils.util.RandomUtilities import ReproducibleRandom
 from Sequential_WC_Simulator.core.simulation_object import EventQueue, SimulationObject
 from Sequential_WC_Simulator.core.simulation_engine import SimulationEngine, MessageTypesRegistry
 from Sequential_WC_Simulator.multialgorithm.specie import Specie
-
+from Sequential_WC_Simulator.multialgorithm.specie import NegativePopulationError
 
 class TestSpecie(unittest.TestCase):
 
     def setUp(self):
         ReproducibleRandom.init( )
 
-    # these tests cover all executable statements in Specie(), including exceptions, and 
-    # all branches
     def test_Specie(self):
         s1 = Specie( 'specie', 10 )
-        
+
         self.assertEqual( s1.get_population( ), 10 )
-        s1.discrete_adjustment( 1 )
+        self.assertEqual( s1.discrete_adjustment( 1, 0 ), 11 )
         self.assertEqual( s1.get_population( ), 11 )
-        s1.discrete_adjustment( -1 )
+        self.assertEqual( s1.discrete_adjustment( -1, 0 ), 10 )
         self.assertEqual( s1.get_population( ), 10 )
+
+        s1 = Specie( 'specie_3', 2, 1 )
+        self.assertEqual( s1.discrete_adjustment( 3, 4 ), 9 )
 
         s1 = Specie( 'specie', 10, initial_flux=0 )
         self.assertEqual( 'specie_name:specie; last_population:10; continuous_time:0; continuous_flux:0', str(s1) )
-        
-        with self.assertRaises(AssertionError) as context:
-            s1.continuous_adjustment( 2, -23, 1 )
-        self.assertIn( 'negative time:', str(context.exception) )
 
-        s1.continuous_adjustment( 2, 4, 1 )
+        with self.assertRaises(ValueError) as context:
+            s1.continuous_adjustment( 2, -23, 1 )
+        self.assertIn( 'continuous_adjustment(): time <= self.continuous_time', str(context.exception) )
+
+        self.assertEqual( s1.continuous_adjustment( 2, 4, 1 ), 12 )
         self.assertEqual( s1.get_population( 4.0 ), 12 )
         self.assertEqual( s1.get_population( 6.0 ), 14 )
 
         with self.assertRaises(ValueError) as context:
             s1.continuous_adjustment( 2, 3, 1 )
         self.assertIn( 'continuous_adjustment(): time <= self.continuous_time', str(context.exception) )
-        
+
         with self.assertRaises(ValueError) as context:
             s1.get_population( )
         self.assertIn( 'get_population(): time needed because continuous adjustment received at time',
             str(context.exception) )
-        
+
         with self.assertRaises(ValueError) as context:
             s1.get_population( 3 )
         self.assertIn( 'get_population(): time < self.continuous_time', str(context.exception) )
-        
-        with self.assertRaises(ValueError) as context:
-            s1.discrete_adjustment( -20 )
-        self.assertIn( 'discrete_adjustment(): negative population: last_population + population_change = ',
-            str(context.exception) )
-
-        with self.assertRaises(ValueError) as context:
-            s1.continuous_adjustment( -22, 5, 2 )
-        self.assertIn( 'continuous_adjustment(): negative population: last_population + population_change = ',
-            str(context.exception) )
 
         s1 = Specie( 'specie', 10 )
         with self.assertRaises(ValueError) as context:
@@ -68,24 +66,64 @@ class TestSpecie(unittest.TestCase):
         with self.assertRaises(AssertionError) as context:
             s1 = Specie( 'specie', -10 )
         self.assertIn( '__init__(): population should be >= 0', str(context.exception) )
-    
+
+    def test_NegativePopulationError(self):
+        s='specie_3'
+        args = ('m', s, 2, -4.0)
+        n1 = NegativePopulationError( *args )
+        self.assertEqual( n1.specie, s )
+        self.assertEqual( n1, NegativePopulationError( *args ) )
+        n1.last_population += 1
+        self.assertNotEqual( n1, NegativePopulationError( *args ) )
+
+        p = "m(): negative population for 'specie_3', with decline from 3 to -1"
+        self.assertEqual( str(n1), p )
+        n1.delta_time=2
+        self.assertEqual( str(n1), p + " over 2 time units" )
+
+    def test_raise_NegativePopulationError(self):
+        s1 = Specie( 'specie_3', 2, -2.0 )
+
+        with self.assertRaises(NegativePopulationError) as context:
+            s1.discrete_adjustment( -3, 0 )
+        self.assertEqual( context.exception, NegativePopulationError('discrete_adjustment', 'specie_3', 2, -3) )
+
+        with self.assertRaises(NegativePopulationError) as context:
+            s1.discrete_adjustment( 0, 3 )
+        self.assertEqual( context.exception, NegativePopulationError('get_population', 'specie_3', 2, -6, 3) )
+
+        with self.assertRaises(NegativePopulationError) as context:
+            s1.continuous_adjustment( -3, 1, 0 )
+        self.assertEqual( context.exception, NegativePopulationError('continuous_adjustment', 'specie_3', 2, -3.0, 1) )
+
+        with self.assertRaises(NegativePopulationError) as context:
+            s1.get_population( 2 )
+        self.assertEqual( context.exception, NegativePopulationError('get_population', 'specie_3', 2, -4.0, 2) )
+
+        s1 = Specie( 'specie_3', 3 )
+        self.assertEqual( s1.get_population( 1 ), 3 )
+
+        with self.assertRaises(NegativePopulationError) as context:
+            s1.discrete_adjustment( -4, 1 )
+        self.assertEqual( context.exception, NegativePopulationError('discrete_adjustment', 'specie_3', 3, -4) )
+
     def test_Specie_stochastic_rounding(self):
         s1 = Specie( 'specie', 10.5 )
-        
+
         samples = 1000
         for i in range(samples):
             pop = s1.get_population( )
             self.assertTrue( pop == 10 or pop == 11 )
-        
+
         mean = sum(s1.get_population( ) for i in range(samples) ) / float(samples)
-        # TODO(Arthur): make sure P[ 10.4 <= mean <= 10.6 ] is high enough 
+        # TODO(Arthur): make sure P[ 10.4 <= mean <= 10.6 ] is high enough
         self.assertTrue( 10.4 <= mean <= 10.6 )
-        
+
         s1 = Specie( 'specie', 10.5, initial_flux=0 )
         s1.continuous_adjustment( 0, 1, 0.25 )
         for i in range(samples):
             self.assertEqual( s1.get_population( 3 ), 11.0 )
-            
+
         ReproducibleRandom.init( seed=123 )
         s2 = Specie( 'specie', 10.5 )
         pops=[]
