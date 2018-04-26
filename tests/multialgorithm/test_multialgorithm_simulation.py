8e06c5a5e05a9db5140f67fa11e9ad65f9b64b75
Arthur Goldberg <artgoldberg@gmail.com>
2018-04-25 22:47:55 -0400

    generate parameters for configuring test models

--- tests/multialgorithm/test_multialgorithm_simulation.py
+++ tests/multialgorithm/test_multialgorithm_simulation.py
@@ -8,19 +8,23 @@
 import os, unittest
 from argparse import Namespace
 import math
+import re
 import numpy as np
 from scipy.constants import Avogadro
 
-from wc_sim.multialgorithm.species_populations import LocalSpeciesPopulation
-from wc_sim.multialgorithm.model_utilities import ModelUtilities
-from wc_sim.multialgorithm.multialgorithm_simulation import MultialgorithmSimulation
 from obj_model import utils
+from wc_utils.util.enumerate import CaseInsensitiveEnum
 from wc_lang.io import Reader
 from wc_lang.core import (Model, Submodel,  SpeciesType, SpeciesTypeType, Species,
                           Reaction, Observable, Compartment,
                           SpeciesCoefficient, ObservableCoefficient, Parameter,
                           RateLaw, RateLawDirection, RateLawEquation, SubmodelAlgorithm, Concentration,
                           BiomassComponent, BiomassReaction, StopCondition)
+from wc_lang.prepare import PrepareModel, CheckModel
+from wc_lang.transform import SplitReversibleReactionsTransform
+from wc_sim.multialgorithm.species_populations import LocalSpeciesPopulation
+from wc_sim.multialgorithm.model_utilities import ModelUtilities
+from wc_sim.multialgorithm.multialgorithm_simulation import MultialgorithmSimulation
 from wc_sim.multialgorithm.config import core as config_core_multialgorithm
 config_multialgorithm = config_core_multialgorithm.get_config()['wc_sim']['multialgorithm']
 
@@ -83,9 +87,42 @@ class TestMultialgorithmSimulation(unittest.TestCase):
         self.assertEqual(len(self.simulation_engine.simulation_objects.keys()), 2)
 
 
+class RateLawType(int, CaseInsensitiveEnum):
+    """ Rate law typ """
+    constant = 1
+    reactant_pop = 2
+    product_pop = 3
+
+
 class TestRunSimulation(unittest.TestCase):
 
+    @staticmethod
+    def get_model_type_params(model_type):
+        num_species = 0
+        result = re.search(r'(\d) species', model_type)
+        if result:
+            num_species = int(result.group(1))
+
+        num_reactions = 0
+        result = re.search(r'(\d) reaction', model_type)
+        if result:
+            num_reactions = int(result.group(1))
+
+        reversible = False
+        if 'pair of symmetrical reactions' in model_type:
+            reversible = True
+            num_reactions = 1
+
+        rate_law_type = RateLawType.constant
+        if 'rates given by reactant population' in model_type:
+            rate_law_type = RateLawType.reactant_pop
+        if 'rates given by product population' in model_type:
+            rate_law_type = RateLawType.product_pop
+
+        return (num_species, num_reactions, reversible, rate_law_type)
+
     def make_test_model(self, model_type):
+        num_species, num_reactions, reversible, rate_law_type = self.get_model_type_params(model_type)
 
         # Model
         self.model = Model(id='test_model', wc_lang_version='0.0.1')
@@ -94,7 +131,7 @@ class TestRunSimulation(unittest.TestCase):
 
         # SpeciesTypes, Species and Concentrations
         self.species = []
-        for i in range(4):
+        for i in range(num_species):
             spec_type = self.model.species_types.create(
                 id='spec_type_{}'.format(i),
                 molecular_weight=10 * (i + 1))
@@ -117,18 +154,54 @@ class TestRunSimulation(unittest.TestCase):
         self.model.parameters.create(id='carbonExchangeRate', value=12, units='mmol/gDCW/h')
         self.model.parameters.create(id='nonCarbonExchangeRate', value=20, units='mmol/gDCW/h')
 
+        # prepare & check the model
+        SplitReversibleReactionsTransform().run(self.model)
+        PrepareModel(self.model).run()
+        CheckModel(self.model).run()
+
         # self.model.pprint(max_depth=3)
         # create Manager indices
         # TODO(Arthur): should be automated in a finalize() method
         for model in [Submodel,  SpeciesType, Reaction, Observable, Compartment, Parameter]:
             model.get_manager().insert_all_new()
 
+    def test_make_test_model(self):
+        '''
+        Simple SSA model tests:
+            no reactions: simulation terminates immediately
+            1 species:
+                one reaction consume specie, at constant rate: consume all reactant, in time given by rate
+            2 species:
+                one reaction: convert all reactant into product, in time given by rate
+                a pair of symmetrical reactions with constant rates: maintain steady state, on average
+                a pair of symmetrical reactions rates given by reactant population: maintain steady state, on average
+                a pair of symmetrical reactions rates given by product population: exhaust on species, with equal chance for each species
+            ** ring of futile reactions with balanced rates: maintain steady state, on average
+        '''
+        model_types = ['no reactions',
+            '1 species, 1 reaction',
+            '2 species, 1 reaction',
+            '2 species, a pair of symmetrical reactions with constant rates',
+            '2 species, a pair of symmetrical reactions rates given by reactant population',
+            '2 species, a pair of symmetrical reactions rates given by product population',
+        ]
+        expected_params_list = [
+            (0, 0, False, RateLawType.constant),
+            (1, 1, False, RateLawType.constant),
+            (2, 1, False, RateLawType.constant),
+            (2, 1, True, RateLawType.constant),
+            (2, 1, True, RateLawType.reactant_pop),
+            (2, 1, True, RateLawType.product_pop)
+        ]
+        for model_type,expected_params in zip(model_types, expected_params_list):
+            params = self.get_model_type_params(model_type)
+            self.assertEqual(params, expected_params)
+
     def setUp(self):
         for base_model in [Submodel, Species, SpeciesType]:
             base_model.objects.reset()
         ### make simple model ###
-        self.make_test_model(None)
-        # SpeciesType.objects._dump_index_dicts()
+        self.make_test_model('2 species, 1 reaction')
         args = Namespace(FBA_time_step=1)
         self.multialgorithm_simulation = MultialgorithmSimulation(self.model, args)
 
