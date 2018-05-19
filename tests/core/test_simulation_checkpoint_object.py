a3f8499d8a4dc816bee8d06f3e2585c5d2ee86ed
Arthur Goldberg <artgoldberg@gmail.com>
2018-05-19 10:22:25 -0400

    fix layout

--- tests/core/test_simulation_checkpoint_object.py
+++ tests/core/test_simulation_checkpoint_object.py
@@ -149,7 +149,7 @@ class TestCheckpointSimulationObjects(unittest.TestCase):
         # check results
         self.assertEqual(expected_num_events, num_events)
         expected_checkpoint_times = [float(t) for t in
-                                     range(0, self.checkpoint_period * int(run_time/self.checkpoint_period) + 1, self.checkpoint_period)]
+            range(0, self.checkpoint_period * int(run_time/self.checkpoint_period) + 1, self.checkpoint_period)]
         checkpoints = Checkpoint.list_checkpoints(self.checkpoint_dir)
         self.assertEqual(expected_checkpoint_times, checkpoints)
         checkpoint = Checkpoint.get_checkpoint(self.checkpoint_dir)
