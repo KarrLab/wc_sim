1dad7343447a8ab6a0016ab50e1e664e89001985
Arthur Goldberg <artgoldberg@gmail.com>
2018-07-24 13:40:54 -0400

    update the example model and the code so a) the model changes mass during the simulation, and b) the code includes a line that obtains the mass of a compartment at a particular time

--- examples/simulation_run.py
+++ examples/simulation_run.py
@@ -21,14 +21,18 @@ results_dir = os.path.expanduser('~/tmp/checkpoints_dir')
 
 # create and run simulation
 simulation = Simulation(model_filename)
-num_events, results_dir = simulation.run(end_time=100, results_dir=results_dir, checkpoint_period=10)
+num_events, results_dir = simulation.run(end_time=30, results_dir=results_dir, checkpoint_period=10)
 run_results = RunResults(results_dir)
 
 # view results
-print(run_results.get('populations'))
-# run_results also contains 'observables', 'aggregate_states', 'random_states', and 'metadata'
+# run_results contains 'populations', 'observables', 'aggregate_states', 'random_states', and 'metadata'
 for component in RunResults.COMPONENTS:
+    print('\ncomponent:', component)
     print(run_results.get(component))
+
+# get the mass of compartment 'c' at time 10
+aggregate_states_df = run_results.get('aggregate_states')
+print("\naggregate_states_df.loc[10, ('c', 'mass')] =", aggregate_states_df.loc[10, ('c', 'mass')])
 
 fig = run_results.get('populations').plot().get_figure()
 fig.savefig(os.path.join(results_dir, 'population_dynamics.pdf'))
