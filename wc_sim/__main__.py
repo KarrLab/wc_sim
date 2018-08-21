029e854a01b1af40c4713f1c4180aa0e8f350fc3
Jonathan Karr <jonrkarr@gmail.com>
2018-08-21 15:28:27 -0400

    updating for cement 3.0.0

--- wc_sim/__main__.py
+++ wc_sim/__main__.py
@@ -23,7 +23,7 @@ class BaseController(cement.Controller):
 
     @cement.ex(hide=True)
     def _default(self):
-        self.app.args.print_help()
+        self._parser.print_help()
 
 
 class App(cement.App):
