588e2108ac01aec0bf8ed16faed3409fd412a224
Jonathan Karr <jonrkarr@gmail.com>
2018-10-23 13:59:51 -0400

    adding example hybrid model

---
+++ examples/transcription_translation_hybrid_model/run_with_custom_code.py
@@ -0,0 +1,264 @@
+""" Use custom code to simulate a model of the transcription, 
+translation, and RNA and protein degradation of 3 genes, each
+with 1 RNA and 1 protein species
+
+:Author: Jonathan Karr <jonrkarr@gmail.com>
+:Date: 2018-10-23
+:Copyright: 2018, Karr Lab
+:License: MIT
+"""
+
+#!/usr/local/bin/python3.6
+
+from matplotlib import pyplot
+import copy
+import numpy
+import scipy.integrate
+
+rxns = {
+    'transcription': {
+        'parts': [{'species': 'rna', 'coefficient': 1}],
+        'rate_law': 'k_transcription',
+        'algorithm': 'ssa',
+    },
+    'translation': {
+        'parts': [{'species': 'prot', 'coefficient': 1}],
+        'rate_law': 'k_translation * (rna / (km_translation + rna))',
+        'algorithm': 'ode',
+    },
+    'rna_deg': {
+        'parts': [{'species': 'rna', 'coefficient': -1}],
+        'rate_law': 'k_rna_deg * rna',
+        'algorithm': 'ssa',
+    },
+    'prot_deg': {
+        'parts': [{'species': 'prot', 'coefficient': -1}],
+        'rate_law': 'k_prot_deg * prot',
+        'algorithm': 'ode',
+    },
+}
+
+rate_params = {
+    'k_transcription': 2. * numpy.log(2) / (3 * 60),
+    'k_translation': 1500 * 2. * numpy.log(2) / (18 * 60 * 60),
+    'km_translation': 2.,
+    'k_rna_deg': numpy.log(2) / (3 * 60),
+    'k_prot_deg': numpy.log(2) / (18 * 60 * 60),
+}
+
+init_species = {
+    'rna': 1,
+    'prot': 1375,
+}
+
+exp_avg_species = {
+    'rna': 2,
+    'prot': 1500,
+}
+
+init_time = 0.
+time_max = 8 * 60 * 60.
+checkpoint_period = 1.
+
+numpy.random.seed(seed=0)
+
+
+def sim_ssa(rxns, rate_params, init_species, init_time, time_max, checkpoint_period):
+    """ Simulate model with SSA
+
+    Args:
+        rxns (:obj:`dict`): reaction participants and rate laws
+        rate_params (:obj:`dict`): values of parameters of rate laws
+        init_species (:obj:`dict` of :obj:`int`):
+            initial copy numbers of RNAs and proteins
+        init_time (:obj:`float`): initial time (seconds)
+        time_max (:obj:`float`): simulation length (seconds)
+        checkpoint_period (:obj:`float`): interval to log simulation results (seconds)
+
+    Returns:
+        :obj:`dict` of :obj:`numpy.ndarray` of :obj:`int`: copy number dynamics of RNAs and proteins
+        :obj:`numpy.ndarray` of :obj:`float`: time (seconds)
+    """
+    rxns = list(rxns.values())
+
+    props = numpy.full((len(rxns),), numpy.nan)
+    species = copy.deepcopy(init_species)
+    time = init_time
+
+    n_log = int((time_max - init_time) / checkpoint_period)
+    species_log = {}
+    time_log = numpy.full((n_log, ), init_time)
+    for species_id, init_val in species.items():
+        species_log[species_id] = numpy.full((n_log, ), init_val)
+
+    while time < time_max:
+        locals = {**species, **rate_params}
+        props = [eval(rxn['rate_law'], locals) for rxn in rxns]
+        tot_prop = numpy.sum(props)
+        if tot_prop == 0:
+            break
+
+        time_step = numpy.random.exponential(1 / tot_prop)
+        time += time_step
+        i_log = int(numpy.ceil((time - init_time) / checkpoint_period))
+        time_log[i_log:] = time
+
+        rxn = numpy.random.choice(rxns, p=props / tot_prop)
+        for part in rxn['parts']:
+            species[part['species']] += part['coefficient']
+            species_log[part['species']][i_log:] = species[part['species']]
+
+    return (species_log, time_log)
+
+
+def sim_ode(rxns, rate_params, init_species, init_time, time_max, checkpoint_period):
+    """ Simulate model with ODE integration
+
+    Args:
+        rxns (:obj:`dict`): reaction participants and rate laws
+        rate_params (:obj:`dict`): values of parameters of rate laws
+        init_species (:obj:`dict` of :obj:`int`):
+            initial copy numbers of RNAs and proteins
+        init_time (:obj:`float`): initial time (seconds)
+        time_max (:obj:`float`): simulation length (seconds)
+        checkpoint_period (:obj:`float`): interval to log simulation results (seconds)
+
+    Returns:
+        :obj:`dict` of :obj:`numpy.ndarray` of :obj:`int`: copy number dynamics of RNAs and proteins
+        :obj:`numpy.ndarray` of :obj:`float`: time (seconds)
+    """
+    time_log = numpy.linspace(init_time, time_max, num=int((time_max-init_time)/checkpoint_period) + 1)
+    species_ids = list(init_species.keys())
+    init_ys = numpy.array([init_species[species_id] for species_id in species_ids])
+
+    def func(ys, time, species_ids, rxns, rate_params):
+        species = {species_id: y for species_id, y in zip(species_ids, ys)}
+        locals = {**species, **rate_params}
+        dys = numpy.zeros(ys.shape)
+        for rxn in rxns.values():            
+            rate = eval(rxn['rate_law'], locals)
+            for part in rxn['parts']:
+                i_species = species_ids.index(part['species'])
+                dys[i_species] += rate * part['coefficient']
+        return dys
+
+    ys_log = scipy.integrate.odeint(func, init_ys, time_log, args=(species_ids, rxns, rate_params))
+    species_log = {species_id: ys_log[:, i_species] for i_species, species_id in enumerate(species_ids)}
+
+    return (species_log, time_log)
+
+
+def sim_hyb(rxns, rate_params, init_species, init_time, time_max, checkpoint_period):
+    """ Simulate model with SSA/ODE hybrid
+
+    Args:
+        rxns (:obj:`dict`): reaction participants and rate laws
+        rate_params (:obj:`dict`): values of parameters of rate laws
+        init_species (:obj:`dict` of :obj:`int`):
+            initial copy numbers of RNAs and proteins
+        init_time (:obj:`float`): initial time (seconds)
+        time_max (:obj:`float`): simulation length (seconds)
+        checkpoint_period (:obj:`float`): interval to log simulation results (seconds)
+
+    Returns:
+        :obj:`dict` of :obj:`numpy.ndarray` of :obj:`int`: copy number dynamics of RNAs and proteins
+        :obj:`numpy.ndarray` of :obj:`float`: time (seconds)
+    """
+    rxns_ssa = []
+    rxns_ode = {}
+    for rxn_id, rxn in rxns.items():
+        if rxn['algorithm'] == 'ssa':
+            rxns_ssa.append(rxn)
+        else:
+            rxns_ode[rxn_id] = rxn
+
+    props = numpy.full((len(rxns_ssa),), numpy.nan)
+    species = copy.deepcopy(init_species)
+    time = init_time
+
+    n_log = int((time_max - init_time) / checkpoint_period)
+    species_log = {}
+    time_log = numpy.full((n_log, ), init_time)
+    for species_id, init_val in species.items():
+        species_log[species_id] = numpy.full((n_log, ), init_val)
+
+    while time < time_max:
+        # select and fire next SSA reaction
+        locals = {**species, **rate_params}
+        props = [eval(rxn['rate_law'], locals) for rxn in rxns_ssa]
+        tot_prop = numpy.sum(props)
+        if tot_prop == 0:
+            break
+
+        time_step = numpy.random.exponential(1 / tot_prop)
+        time += time_step
+        i_log = int(numpy.ceil((time - init_time) / checkpoint_period))
+        time_log[i_log:] = time
+
+        rxn = numpy.random.choice(rxns_ssa, p=props / tot_prop)
+        for part in rxn['parts']:
+            species[part['species']] += part['coefficient']
+            species_log[part['species']][i_log:] = species[part['species']]
+
+        # interpolate ODE
+        ode_species_log, _ = sim_ode(rxns_ode, rate_params, species, 0, time_step, time_step)
+        for species_id in species.keys():
+            species[species_id] = ode_species_log[species_id][-1]
+            species_log[species_id][i_log:] = species[species_id]
+
+    return (species_log, time_log)
+
+
+def plot(
+    species_ssa, time_ssa, 
+    species_ode, time_ode, 
+    species_hyb, time_hyb, 
+    exp_avg_species, filename):
+    fig, axes = pyplot.subplots(nrows=2, ncols=1)
+
+    # RNA
+    avg = numpy.mean(species_ssa['rna'])
+    axes[0].plot(time_ssa / 3600, species_ssa['rna'], label='SSA <{0:.1f}>'.format(avg))
+    
+    avg = numpy.mean(species_ode['rna'])
+    axes[0].plot(time_ode / 3600, species_ode['rna'], label='ODE <{0:.1f}>'.format(avg))
+    
+    avg = numpy.mean(species_hyb['rna'])
+    axes[0].plot(time_hyb / 3600, species_hyb['rna'], label='Hybrid <{0:.1f}>'.format(avg))
+    
+    axes[0].plot([0, time_ssa[-1] / 3600], [avg, avg], label='Avg: {}'.format(exp_avg_species['rna']))
+    axes[0].set_xlim((time_ssa[0] / 3600, time_ssa[-1] / 3600))
+    axes[0].set_ylim((0., 10.0))
+    #axes[0].set_xlabel('Time (h)')
+    axes[0].set_ylabel('RNA (molecules)')
+    axes[0].legend(loc='upper right')
+
+    # Protein
+    avg = numpy.mean(species_ssa['prot'])
+    axes[1].plot(time_ssa / 3600, species_ssa['prot'], label='SSA <{0:.0f}>'.format(avg))
+    
+    avg = numpy.mean(species_ode['prot'])
+    axes[1].plot(time_ode / 3600, species_ode['prot'], label='ODE <{0:.0f}>'.format(avg))
+    
+    avg = numpy.mean(species_hyb['prot'])
+    axes[1].plot(time_hyb / 3600, species_hyb['prot'], label='Hybrid <{0:.0f}>'.format(avg))
+
+    axes[1].plot([0, time_ssa[-1] / 3600], [avg, avg], label='Avg: {}'.format(exp_avg_species['prot']))
+    axes[1].set_xlim((time_ssa[0] / 3600, time_ssa[-1] / 3600))
+    axes[1].set_ylim((1300., 1700.))
+    axes[1].set_xlabel('Time (h)')
+    axes[1].set_ylabel('Protein (molecules)')
+    axes[1].legend(loc='upper right')
+
+    fig.savefig(filename)
+    pyplot.close(fig)
+
+
+ssa_species_log, ssa_time_log = sim_ssa(rxns, rate_params, init_species, init_time, time_max, checkpoint_period)
+ode_species_log, ode_time_log = sim_ode(rxns, rate_params, init_species, init_time, time_max, checkpoint_period)
+hyb_species_log, hyb_time_log = sim_hyb(rxns, rate_params, init_species, init_time, time_max, checkpoint_period)
+plot(ssa_species_log, ssa_time_log, 
+    ode_species_log, ode_time_log, 
+    hyb_species_log, hyb_time_log, 
+    exp_avg_species, 'custom_results.pdf')
+
---
+++ examples/transcription_translation_hybrid_model/run_with_wc_sim.py
@@ -0,0 +1,101 @@
+""" Use wc_sim to simulate a model of the transcription, 
+translation, and RNA and protein degradation of 3 genes, each
+with 1 RNA and 1 protein species
+
+:Author: Jonathan Karr <jonrkarr@gmail.com>
+:Date: 2018-10-23
+:Copyright: 2018, Karr Lab
+:License: MIT
+"""
+
+from matplotlib import pyplot
+from wc_sim.multialgorithm.simulation import Simulation
+from wc_sim.multialgorithm.run_results import RunResults
+import numpy
+import wc_lang.io
+
+model_filename = 'model.xlsx'
+results_parent_dirname = 'results'
+checkpoint_period = 100.
+
+# read model
+model = wc_lang.io.Reader().run(model_filename)
+
+# run simulation
+sim = Simulation(model)
+_, results_dirname = sim.run(end_time=model.parameters.get_one(id='cell_cycle_len').value,
+                             results_dir=results_parent_dirname,
+                             checkpoint_period=checkpoint_period)
+results = RunResults(results_dirname)
+
+# plot results
+
+
+def plot(model, results, filename):
+    # get expected results
+    c = model.compartments.get_one(id='c')
+
+    species_type = model.species_types.get_one(id='rna')
+    species = species_type.species.get_one(compartment=c)
+    exp_avg_rna = species.concentration.value
+
+    species_type = model.species_types.get_one(id='prot')
+    species = species_type.species.get_one(compartment=c)
+    exp_avg_prot = species.concentration.value
+
+    # get simulation results
+    pops = results.get('populations')
+    time = pops.index
+    rna = pops['rna[c]']
+    prot = pops['prot[c]']
+
+    states = results.get('aggregate_states')
+    mass = states['c']['mass']
+    vol = states['c']['volume']
+
+    # initialize figure
+    fig1, axes1 = pyplot.subplots(nrows=2, ncols=1)
+    fig2, axes2 = pyplot.subplots(nrows=2, ncols=1)
+
+    # plot RNA predictions
+    avg = numpy.mean(rna)
+    axes1[0].plot(time / 3600, rna, label='<{0:.1f}>'.format(avg))
+    axes1[0].plot([0, time[-1] / 3600], [avg, avg], label='Avg: {}'.format(exp_avg_rna))
+    axes1[0].set_xlim((time[0] / 3600, time[-1] / 3600))
+    axes1[0].set_ylim((0., 10.0))
+    #axes1[0].set_xlabel('Time (h)')
+    axes1[0].set_ylabel('RNA (molecules)')
+    axes1[0].legend(loc='upper right')
+
+    # plot protein predictions
+    avg = numpy.mean(prot)
+    axes1[1].plot(time / 3600, prot, label='<{0:.0f}>'.format(avg))
+    axes1[1].plot([0, time[-1] / 3600], [avg, avg], label='Avg: {}'.format(exp_avg_prot))
+    axes1[1].set_xlim((time[0] / 3600, time[-1] / 3600))
+    axes1[1].set_ylim((1300., 1700.))
+    axes1[1].set_xlabel('Time (h)')
+    axes1[1].set_ylabel('Protein (molecules)')
+    axes1[1].legend(loc='upper right')
+
+    # plot mass
+    axes2[0].plot(time / 3600, mass * 1e15)
+    axes2[0].set_xlim((time[0] / 3600, time[-1] / 3600))
+    #axes2[0].set_ylim((0, 2 * 51))
+    axes2[0].set_xlabel('Time (h)')
+    axes2[0].set_ylabel('Mass (fg)')
+
+    # plot volume
+    axes2[1].plot(time / 3600, vol * 1e18)
+    axes2[1].set_xlim((time[0] / 3600, time[-1] / 3600))
+    #axes2[1].set_ylim((0, 2 * 67))
+    axes2[1].set_xlabel('Time (h)')
+    axes2[1].set_ylabel('Volume (aL)')
+
+    # save and close figure
+    fig1.savefig(filename.format('species'))
+    fig2.savefig(filename.format('mass'))
+    pyplot.close(fig1)
+    pyplot.close(fig2)
+
+
+plot(model, results, 'wc_sim_results-{}.pdf')
