238a90c45e1adc49c12ad8bb7e8d4842f733f343
Arthur Goldberg <artgoldberg@gmail.com>
2019-06-19 08:51:16 -0400

    CLI in __main__.py, and automated migration configuration file for wc_lang schema

---
+++ migrate/__main__.py
@@ -0,0 +1,13 @@
+""" Add migrate CLI to wc_sim
+
+:Author: Arthur Goldberg <Arthur.Goldberg@mssm.edu>
+:Date: 2019-05-22
+:Copyright: 2019, Karr Lab
+:License: MIT
+"""
+
+from obj_model.migrate import data_repo_main
+
+
+if __name__ == '__main__':
+    data_repo_main()
