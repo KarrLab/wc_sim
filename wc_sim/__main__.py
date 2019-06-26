655fa4bdd20b0a03c030088a8960a74dea8383db
Arthur Goldberg <artgoldberg@gmail.com>
2019-06-25 22:16:45 -0400

    changes for wc_cli

--- wc_sim/__main__.py
+++ wc_sim/__main__.py
@@ -1,11 +1,14 @@
 """ Command line programs for simulating whole-cell models
 
 :Author: Jonathan Karr <karr@mssm.edu>
+:Author: Arthur Goldberg <Arthur.Goldberg@mssm.edu>
 :Date: 2018-05-15
 :Copyright: 2018, Karr Lab
 :License: MIT
 """
 
+from obj_model.migrate import data_repo_migration_controllers
+from obj_model.migrate import CementControllers
 from .multialgorithm.__main__ import handlers as multialgorithm_handlers
 import cement
 import wc_sim
@@ -31,7 +34,7 @@ class App(cement.App):
     class Meta:
         label = 'wc-sim'
         base_controller = 'base'
-        handlers = [BaseController,] + multialgorithm_handlers
+        handlers = [BaseController] + multialgorithm_handlers + data_repo_migration_controllers
 
 
 def main():
