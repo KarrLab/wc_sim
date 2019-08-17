10fac1187e85bdc23c54b6cd8d00b9463f1f0ed0
Arthur Goldberg <artgoldberg@gmail.com>
2019-08-17 08:58:28 -0400

    remove unused cfg file; more test code

--- tests/examples/test_minimal_simulation.py
+++ tests/examples/test_minimal_simulation.py
@@ -14,7 +14,9 @@ from de_sim.config.debug_logs import config
 
 from pprint import pprint
 import sys
+print('config')
 pprint(config)
+print('config', file=sys.stderr)
 pprint(config, stream=sys.stderr)
 
 class TestMinimalSimulation(unittest.TestCase):
