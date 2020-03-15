d96714fd64b977a298134589ced9892fec466af8
Jonathan Karr <jonrkarr@gmail.com>
2020-03-15 15:26:01 -0400

    finishing updating fixtures for changes to obj_tables and wc_lang

--- tests/test_wc_lang_files_are_valid.py
+++ tests/test_wc_lang_files_are_valid.py
@@ -43,17 +43,83 @@ class ValidateWcLangFilesTestCase(unittest.TestCase):
          'ignore_extra_models': True},
         {'path': 'tests/submodels/fixtures/test_submodel.xlsx'},
         {'path': 'tests/submodels/fixtures/test_submodel_no_shared_species.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/multialgorithmic/00001/00001-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/multialgorithmic/00003/00003-wc_lang_1_submodel.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/multialgorithmic/00003/00003-wc_lang_2_submodels.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/multialgorithmic/00003/00003-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/multialgorithmic/00007/00007-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/multialgorithmic/00020/00020-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/multialgorithmic/00021/00021-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/multialgorithmic/00030/00007-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/multialgorithmic/00030/00030-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/semantic/00001/00001-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/semantic/00002/00002-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/semantic/00003/00003-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/semantic/00004/00004-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/semantic/00005/00005-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/semantic/00006/00006-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/semantic/00010/00010-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/semantic/00014/00014-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/semantic/00015/00015-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/semantic/00017/00017-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/semantic/00018/00018-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/semantic/00019/00019-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/semantic/00020/00020-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/semantic/00021/00021-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/semantic/00022/00022-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/semantic/00028/00028-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/semantic/00054/00054-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/stochastic/00001/00001-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/stochastic/00003/00003-wc_lang_1_submodel.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/stochastic/00003/00003-wc_lang_2_submodels.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/stochastic/00003/00003-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/stochastic/00004/00004-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/stochastic/00007/00007-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/stochastic/00007_hybrid/00007_hybrid-wc_lang_old.xlsx',
+        'validate': False},
+        {'path': 'tests/fixtures/verification/cases/stochastic/00007_hybrid/00007_hybrid-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/stochastic/00012/00012-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/stochastic/00020/00020-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/stochastic/00021/00021-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/stochastic/00030/00030-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/stochastic/00037/00037-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/cases/stochastic/transcription_translation/transcription_translation-wc_lang.xlsx',
+        'validate': False},
+        {'path': 'tests/fixtures/verification/testing/hybrid/transcription_translation/transcription_translation_correct_ssa.xlsx',
+        'validate': False},
+        {'path': 'tests/fixtures/verification/testing/hybrid/transcription_translation/transcription_translation_hybrid.xlsx',
+        'validate': False},
+        {'path': 'tests/fixtures/verification/testing/hybrid/transcription_translation/transcription_translation-wc_lang_JK.xlsx',
+        'validate': False},
+        {'path': 'tests/fixtures/verification/testing/hybrid/translation_metabolism/translation_metabolism_correct_ssa.xlsx',
+        'validate': False},
+        {'path': 'tests/fixtures/verification/testing/hybrid/translation_metabolism/translation_metabolism_hybrid.xlsx',
+        'validate': False},
+        {'path': 'tests/fixtures/verification/testing/multialgorithmic/00007/00007-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/testing/semantic/00001/00001-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/testing/semantic/00004/00004-wc_lang.xlsx',
+        'validate': False},
+        {'path': 'tests/fixtures/verification/testing/semantic/00054/00054-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/testing/stochastic/00001/00001-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/testing/stochastic/00006/00006-wc_lang.xlsx'},
+        {'path': 'tests/fixtures/verification/testing_ValidationSuite_run/stochastic/00001/00001-wc_lang.xlsx',
+        'validate': False},
+        {'path': 'tests/fixtures/verification/testing_ValidationSuite_run/stochastic/00006/00006-wc_lang.xlsx',
+        'validate': False},
     )
 
     def test(self):
         errs = []
         for file in self.FILES:
-            kwargs = copy.copy(file)
-            kwargs.pop('path')
-            try:
-                # reader already does validation
-                wc_lang.io.Reader().run(file['path'], validate=True, **kwargs)
-            except ValueError as err:
-                errs.append("File: {}\n{}".format(file['path'], str(err)))
+            if file.get('validate', True):
+                kwargs = copy.copy(file)
+                kwargs.pop('path')
+                if 'validate' in kwargs:
+                    kwargs.pop('validate')
+                try:
+                    # reader already does validation
+                    wc_lang.io.Reader().run(file['path'], validate=True, **kwargs)
+                except ValueError as err:
+                    errs.append("File: {}\n{}".format(file['path'], str(err)))
         if errs:
             raise Exception('The following example model(s) are invalid:\n{}'.format('\n'.join(errs)))
