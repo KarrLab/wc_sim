6653047c16caa73acf87816206f9c976a4087812
Jonathan Karr <jonrkarr@gmail.com>
2020-03-15 14:52:32 -0400

    updating test that model files are valid

--- tests/test_wc_lang_files_are_valid.py
+++ tests/test_wc_lang_files_are_valid.py
@@ -7,31 +7,53 @@
 :License: MIT
 """
 
+import copy
 import unittest
 import wc_lang.io
 
 
 class ValidateWcLangFilesTestCase(unittest.TestCase):
     FILES = (
-        'tests/submodels/fixtures/test_submodel_no_shared_species.xlsx',
-        'tests/fixtures/test_dry_model.xlsx',
-        'tests/fixtures/test_model.xlsx',
-        'tests/fixtures/MetabolismAndGeneExpression.xlsx',
-        'tests/fixtures/test_dry_model_with_mass_computation.xlsx',
-        'tests/fixtures/2_species_1_reaction.xlsx',
-        'tests/fixtures/test_model_for_access_species_populations.xlsx',
-        'tests/fixtures/test_model_for_access_species_populations_steady_state.xlsx',
-        'examples/transcription_translation_hybrid_model/model.xlsx',
-        'examples/translation_metabolism_hybrid_model/model.xlsx'
+        {'path': 'examples/transcription_translation_hybrid_model/model.xlsx'},
+        {'path': 'examples/translation_metabolism_hybrid_model/model.xlsx'},
+        {'path': 'tests/fixtures/2_species_1_reaction.xlsx'},
+        {'path': 'tests/fixtures/2_species_1_reaction_with_rates_given_by_reactant_population.xlsx'},
+        {'path': 'tests/fixtures/2_species_a_pair_of_symmetrical_reactions_rates_given_by_reactant_population.xlsx'},
+        {'path': 'tests/fixtures/MetabolismAndGeneExpression.xlsx'},
+        {'path': 'tests/fixtures/test_dry_model.xlsx'},
+        {'path': 'tests/fixtures/test_dry_model_with_mass_computation.xlsx',
+         'ignore_extra_models': True},
+        {'path': 'tests/fixtures/test_dynamic_expressions.xlsx'},
+        {'path': 'tests/fixtures/test_model.xlsx',
+         'ignore_extra_models': True},
+        {'path': 'tests/fixtures/test_model_for_access_species_populations.xlsx'},
+        {'path': 'tests/fixtures/test_model_for_access_species_populations_steady_state.xlsx'},
+        {'path': 'tests/fixtures/test_new_features_model.xlsx'},
+        {'path': 'tests/fixtures/dynamic_tests/one_exchange_rxn_compt_growth.xlsx',
+         'ignore_extra_models': True},
+        {'path': 'tests/fixtures/dynamic_tests/stop_conditions.xlsx',
+         'ignore_extra_models': True},
+        {'path': 'tests/fixtures/dynamic_tests/one_reaction_linear.xlsx',
+         'ignore_extra_models': True},
+        {'path': 'tests/fixtures/dynamic_tests/template.xlsx',
+         'ignore_extra_models': True},
+        {'path': 'tests/fixtures/dynamic_tests/one_rxn_exponential.xlsx',
+         'ignore_extra_models': True},
+        {'path': 'tests/fixtures/dynamic_tests/static.xlsx',
+         'ignore_extra_models': True},
+        {'path': 'tests/submodels/fixtures/test_submodel.xlsx'},
+        {'path': 'tests/submodels/fixtures/test_submodel_no_shared_species.xlsx'},
     )
 
     def test(self):
         errs = []
         for file in self.FILES:
+            kwargs = copy.copy(file)
+            kwargs.pop('path')
             try:
                 # reader already does validation
-                wc_lang.io.Reader().run(file, validate=True, ignore_extra_models=True)
+                wc_lang.io.Reader().run(file['path'], validate=True, **kwargs)
             except ValueError as err:
-                errs.append("File: {}\n{}".format(file, str(err)))
+                errs.append("File: {}\n{}".format(file['path'], str(err)))
         if errs:
             raise Exception('The following example model(s) are invalid:\n{}'.format('\n'.join(errs)))
